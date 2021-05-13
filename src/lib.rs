57cf5d60999be26dfbd5ca3e84184ea26564d668
Jared De La Cruz <jared_de_la_cruz@yahoo.com>
2021-05-13 15:23:35 -0700

    Change to wasm

--- src/lib.rs
+++ src/lib.rs
@@ -1,19 +1,23 @@
-#[allow(improper_ctypes_definitions)]
 pub mod gut_time {
+    use std::ffi::CString;
+    use std::os::raw::c_char;
     use std::time::{SystemTime, UNIX_EPOCH};
 
     #[no_mangle]
-    pub extern "C" fn gut_export_functions() -> String {
-        r#"["time_now"]"#.to_string()
+    pub extern "C" fn gut_export_functions() -> *mut c_char {
+        let c_string = CString::new(r#"["time_now"]"#).expect("Failed to create c_string");
+        c_string.into_raw()
     }
 
     #[no_mangle]
-    pub extern "C" fn gut_export_descriptions() -> String {
-        r#"["Prints the current time in milliseconds"]"#.to_string()
+    pub extern "C" fn gut_export_descriptions() -> *mut c_char {
+        let c_string = CString::new(r#"["Prints the current time in milliseconds"]"#)
+            .expect("Failed to create c_string");
+        c_string.into_raw()
     }
 
     #[no_mangle]
-    fn time_now() {
+    fn time_now(_ptr: i32, _len: i32) {
         let now = SystemTime::now();
         let epoch = now.duration_since(UNIX_EPOCH).expect("Failed to get time");
 
