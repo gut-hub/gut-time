4281eddb4a42db05b26aad96aaeefd07a7624074
Jared De La Cruz <jared_de_la_cruz@yahoo.com>
2021-05-17 22:26:40 -0700

    Use gut-plugin

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,11 @@
-pub mod gut_time {
-    use std::ffi::CString;
-    use std::os::raw::c_char;
-    use std::time::{SystemTime, UNIX_EPOCH};
+use std::time::{SystemTime, UNIX_EPOCH};
 
-    #[no_mangle]
-    pub extern "C" fn gut_export_functions() -> *mut c_char {
-        let c_string = CString::new(r#"["time_now"]"#).expect("Failed to create c_string");
-        c_string.into_raw()
-    }
+gut_plugin::gut_export!(["time_now"], ["Prints the current time in milliseconds"]);
 
-    #[no_mangle]
-    pub extern "C" fn gut_export_descriptions() -> *mut c_char {
-        let c_string = CString::new(r#"["Prints the current time in milliseconds"]"#)
-            .expect("Failed to create c_string");
-        c_string.into_raw()
-    }
+#[no_mangle]
+fn time_now(_ptr: i32, _len: i32) {
+    let now = SystemTime::now();
+    let epoch = now.duration_since(UNIX_EPOCH).expect("Failed to get time");
 
-    #[no_mangle]
-    fn time_now(_ptr: i32, _len: i32) {
-        let now = SystemTime::now();
-        let epoch = now.duration_since(UNIX_EPOCH).expect("Failed to get time");
-
-        println!("{}", epoch.as_millis())
-    }
+    println!("{}", epoch.as_millis())
 }
