cce8620fc3df19caa5e13905e4c3f82d977e95a3
Jared De La Cruz <jared_de_la_cruz@yahoo.com>
2021-05-19 14:30:50 -0700

    Change fn signature

--- src/lib.rs
+++ src/lib.rs
@@ -3,7 +3,7 @@ use std::time::{SystemTime, UNIX_EPOCH};
 gut_plugin::gut_export!(["time_now"], ["Prints the current time in milliseconds"]);
 
 #[no_mangle]
-fn time_now(_ptr: i32, _len: i32) {
+fn time_now(_ptr: *mut c_char) {
     let now = SystemTime::now();
     let epoch = now.duration_since(UNIX_EPOCH).expect("Failed to get time");
 
